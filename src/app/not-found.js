88c17c6be6ce98a9a045abfc0c4af103885e88a7
Maria Soledad Lanfranconi <pennymsl@gmail.com>
2023-09-05 17:18:52 -0300

    feat: not found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,9 +4,14 @@ import LayoutWrapper from './components/LayoutWrapper'
 export default function NotFound() {
   return (
     <LayoutWrapper>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <section className="h-96 flex flex-col justify-center items-center">
+        <figure>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-frown h-8 w-8 text-rose-900"><circle cx="12" cy="12" r="10"/><path d="M16 16s-1.5-2-4-2-4 2-4 2"/><line x1="9" x2="9.01" y1="9" y2="9"/><line x1="15" x2="15.01" y1="9" y2="9"/></svg>
+        </figure>
+        <h2 className="font-bold text-3xl mt-2">SORRY</h2>
+        <p>We couldn't find that page</p>
+        <Link href="/" className="bg-rose-900 text-white text-xs p-1 w-36 rounded-lg text-center mt-4 py-2">Return Home</Link>
+      </section>
     </LayoutWrapper>
   )
 }
